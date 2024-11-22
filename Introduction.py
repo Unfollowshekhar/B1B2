b94da65e20642e9e8545f437ce7dd6d8b6058540
Shekhar Kumar Jha <Shekhar.11163@stu.upes.ac.in>
2024-11-22 13:11:25 +0530

    Assignment 1 ( Re Uploaded )
    The previous file was not opening properly so i am reuploading it again , you can se few files of Assignment 1 uploaded 2 months ago ! Kindly Reconsider

---
+++ Introduction.py
@@ -0,0 +1,6 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("My Name: Shekhar Kumar Jha")
+My Name: Shekhar Kumar Jha
+>>> print("Course Name: Master Of Computer Application(MCA)")
+Course Name: Master Of Computer Application(MCA)
